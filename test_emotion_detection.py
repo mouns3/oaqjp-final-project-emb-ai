51acf83e4523a017d9e5c395307e062a6ad6a3cb
mouns3 <adrien.ferrando@outlook.fr>
2026-07-07 05:42:56 -0400

    Add unit tests for emotion detector

---
+++ test_emotion_detection.py
@@ -0,0 +1,52 @@
+"""
+Unit tests for the EmotionDetection package.
+"""
+
+import unittest
+
+from EmotionDetection import emotion_detector
+
+
+class TestEmotionDetector(unittest.TestCase):
+    """
+    Test cases for the emotion_detector function.
+    """
+
+    def test_joy(self):
+        """
+        Test that joy is detected as the dominant emotion.
+        """
+        result = emotion_detector("I am glad this happened")
+        self.assertEqual(result["dominant_emotion"], "joy")
+
+    def test_anger(self):
+        """
+        Test that anger is detected as the dominant emotion.
+        """
+        result = emotion_detector("I am really mad about this")
+        self.assertEqual(result["dominant_emotion"], "anger")
+
+    def test_disgust(self):
+        """
+        Test that disgust is detected as the dominant emotion.
+        """
+        result = emotion_detector("I feel disgusted just hearing about this")
+        self.assertEqual(result["dominant_emotion"], "disgust")
+
+    def test_sadness(self):
+        """
+        Test that sadness is detected as the dominant emotion.
+        """
+        result = emotion_detector("I am so sad about this")
+        self.assertEqual(result["dominant_emotion"], "sadness")
+
+    def test_fear(self):
+        """
+        Test that fear is detected as the dominant emotion.
+        """
+        result = emotion_detector("I am really afraid that this will happen")
+        self.assertEqual(result["dominant_emotion"], "fear")
+
+
+if __name__ == "__main__":
+    unittest.main()
